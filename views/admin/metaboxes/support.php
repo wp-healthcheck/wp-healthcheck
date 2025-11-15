326acfb465dd7d616efd85b56aa91a165907c2ab
Tiago Hillebrandt <tiago@tiagohillebrandt.com>
2025-11-15 16:43:24 -0500

    Fixes PHPCS violations.

--- views/admin/metaboxes/support.php
+++ views/admin/metaboxes/support.php
@@ -6,22 +6,28 @@ if ( ! defined( 'WPHC' ) ) {
 
 <p>
 	<?php
-	/* translators: %1$s is the URL to our community forum */
-	echo sprintf( __( 'If you have any questions, you can post a new thread in our <a href="%1$s">Community Forum</a>, available on WordPress.org. We review it weekly and our team will be happy to assist you there.', 'wp-healthcheck' ), 'https://wordpress.org/support/plugin/wp-healthcheck' );
+	/* translators: %1$s is the URL to our community forum. */
+	printf(
+		wp_kses_post( __( 'If you have any questions, you can post a new thread in our <a href="%1$s">Community Forum</a>, available on WordPress.org. We review it weekly and our team will be happy to assist you there.', 'wp-healthcheck' ) ),
+		esc_url( 'https://wordpress.org/support/plugin/wp-healthcheck' )
+	);
 	?>
 </p>
 
-<p><?php _e( '<strong>Looking for professional services?</strong> We can help you to review your server configuration, improve your site performance, or even build a high-performance server from scratch to host your sites.', 'wp-healthcheck' ); ?></p>
+<p><?php esc_html_e( 'Looking for professional services? We can help you to review your server configuration, improve your site performance, or even build a high-performance server from scratch to host your sites.', 'wp-healthcheck' ); ?></p>
 
 <p>
 	<?php
-	/* translators: %1$s is the URL to our premium services page */
-	echo sprintf( __( 'You can read more details about our services and hire us through our <a href="%1$s">Premium Services</a> area.', 'wp-healthcheck' ), 'https://wp-healthcheck.com/services' );
+	/* translators: %1$s is the URL to our premium services page. */
+	printf(
+		wp_kses_post( __( 'You can read more details about our services and hire us through our <a href="%1$s">Premium Services</a> area.', 'wp-healthcheck' ) ),
+		esc_url( 'https://wp-healthcheck.com/services' )
+	);
 	?>
 </p>
 
 <div class="wphc_support_btns">
-	<a href="https://wp-healthcheck.com" class="button"><span class="wphc-icons-home"></span> <?php _e( 'Home Page', 'wp-healthcheck' ); ?></a>&nbsp;
-	<a href="https://wp-healthcheck.com/contributors" class="button"><span class="wphc-icons-contributors"></span> <?php _e( 'Contributors', 'wp-healthcheck' ); ?></a>&nbsp;
-	<a href="https://github.com/wp-healthcheck/wp-healthcheck/issues" class="button"><span class="wphc-icons-bug-report"></span> <?php _e( 'Report a Bug', 'wp-healthcheck' ); ?></a>
+	<a href="<?php echo esc_url( 'https://wp-healthcheck.com' ); ?>" class="button"><span class="wphc-icons-home"></span> <?php esc_html_e( 'Home Page', 'wp-healthcheck' ); ?></a>&nbsp;
+	<a href="<?php echo esc_url( 'https://wp-healthcheck.com/contributors' ); ?>" class="button"><span class="wphc-icons-contributors"></span> <?php esc_html_e( 'Contributors', 'wp-healthcheck' ); ?></a>&nbsp;
+	<a href="<?php echo esc_url( 'https://github.com/wp-healthcheck/wp-healthcheck/issues' ); ?>" class="button"><span class="wphc-icons-bug-report"></span> <?php esc_html_e( 'Report a Bug', 'wp-healthcheck' ); ?></a>
 </div>
--- views/notices/mysql.php
+++ views/notices/mysql.php
@@ -3,32 +3,32 @@ if ( ! defined( 'WPHC' ) ) {
 	exit;
 }
 
-$status = wphc( 'module.server' )->is_updated( 'mysql' );
+$mysql_status = wphc( 'module.server' )->is_updated( 'mysql' );
 
-if ( 'updated' == $status || false === $status ) {
+if ( $mysql_status === 'updated' || $mysql_status === false ) {
 	return false;
 }
 
 $requirements = wphc( 'module.server' )->get_requirements();
 $server_data  = wphc( 'module.server' )->get_data();
 
-$messages = array(
-	'outdated' => array(
+$messages = [
+	'outdated' => [
 		'class'   => 'notice-warning is-dismissible',
-		/* translators: %1$s is the MySQL version installed on server, %2$s is the one WP team recommends */
+		/* translators: %1$s is the MySQL version installed on server, %2$s is the one WP team recommends. */
 		'message' => sprintf( __( 'Your MySQL version (%1$s) is compatible with the current WordPress install. However, the <a href="https://wordpress.org/about/requirements/">WordPress team recommends</a> you upgrade your server to MySQL %2$s or greater.', 'wp-healthcheck' ), $server_data['mysql'], $requirements['mysql']['recommended'] ),
-	),
-	'obsolete' => array(
+	],
+	'obsolete' => [
 		'class'   => 'notice-error is-dismissible',
-		/* translators: %1$s is the MySQL version installed on server, %2$s is the one WP team recommends */
+		/* translators: %1$s is the MySQL version installed on server, %2$s is the one WP team recommends. */
 		'message' => sprintf( __( 'The MySQL version you are using (%1$s) is not supported by WordPress anymore! Please contact your developers and/or hosting company to upgrade your MySQL to version %2$s or greater.', 'wp-healthcheck' ), $server_data['mysql'], $requirements['mysql']['recommended'] ),
-	),
-);
+	],
+];
 ?>
 
-<div class="notice wphc-notice wphc-notice-mysql <?php echo $messages[ $status ]['class']; ?>">
+<div class="notice wphc-notice wphc-notice-mysql <?php echo esc_attr( $messages[ $mysql_status ]['class'] ); ?>">
 	<p>
-		<strong>WP Healthcheck:</strong>
-		<?php echo $messages[ $status ]['message']; ?>
+		<strong><?php esc_html_e( 'WP Healthcheck:', 'wp-healthcheck' ); ?></strong>
+		<?php echo wp_kses_post( $messages[ $mysql_status ]['message'] ); ?>
 	</p>
 </div>
